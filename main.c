9a1bf7f189a0bc8c69668f0ff9edaedc3c2d12dc
Tejaswi-MN <m75tejaswi@gmail.com>
2026-09-04 04:23:21 +0000

    added validation step

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "apc.h"
+#include "input.h"
+#include "addition.h"
+#include "subtraction.h"
+#include "multiplication.h"
+#include "division.h"
+
+int main(int argc, char *argv[])
+{
+    Dlist *head1 = NULL;
+    Dlist *tail1 = NULL;
+
+    Dlist *head2 = NULL;
+    Dlist *tail2 = NULL;
+
+    Dlist *headR = NULL;
+    Dlist *tailR = NULL;
+
+    return 0;
+}
---
+++ types.h
@@ -0,0 +1,12 @@
+#ifndef TYPES_H
+#define TYPES_H
+#define SUCCESS 0
+#define FAILURE -1
+
+typedef struct node 
+{
+    int data;
+    struct node* prev;
+    struct node* next;
+}List;
+#endif
